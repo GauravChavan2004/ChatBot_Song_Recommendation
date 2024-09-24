fe2a03f27905302ca3bc69e64907c665da6d383d
GauravChavan2004 <gauravchavan101004@gmail.com>
2024-09-24 23:59:50 +0530

    change front-end

--- ChatBot_Song/settings.py
+++ ChatBot_Song/settings.py
@@ -117,12 +117,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR , "static",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-    BASE_DIR , "static",
-]
+
+
+# Base url to serve media files
+MEDIA_URL = '/media/'
+
+# Path where media is stored'
+MEDIA_ROOT = BASE_DIR / 'media'
